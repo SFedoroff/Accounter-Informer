1c5712b2f0cb7f42020d04f86ae0a2360ee7482a
Vyacheslav Fedorov <slava.fedoroff@mail.ru>
2020-02-04 16:44:02 +0300

    Initial Commit

---
+++ Account Informer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Account Informer
+//
+//  Created by Vyacheslav on 04.02.2020.
+//  Copyright © 2020 Business Systems. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
